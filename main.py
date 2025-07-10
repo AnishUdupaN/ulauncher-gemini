164a0667de5a34a95a4aa42c531a94d6291e9795
AnishUdupaN <anish@email>
2025-07-10 20:04:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+class ClipboardHistoryExtension(Extension):
+    def __init__(self):
+        super(ClipboardHistoryExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        items = []
+        stringinput = event.get_argument() or ""
+        persistent = self.preferences["persistent"]
+        items.append(ExtensionResultItem(
+            icon=os.path.join(os.getcwd(),'images/icon.png'),
+            name='Hello World',
+            description="Click to Open",
+            on_enter=RunScriptAction(f'xdg-open "{lines[i]}"', [])
+        ))
+        return RenderResultListAction(items[:1])
+
+if __name__ == '__main__':
+    ClipboardHistoryExtension().run()
